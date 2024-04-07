86401cd49e55dbb6bd9beeea07dc523dfad49682
sbn raju <simhardrinagaraju@gmail.com>
2024-04-07 17:19:06 +0530

    All Files Uploading

--- src/controllers/projectPage.controllers.js
+++ src/controllers/projectPage.controllers.js
@@ -1,56 +1,56 @@
 const getprojectPageContorller = (req,res)=>{
-    // let projectID = req.params.id;
-    // switch(projectID){
-    //     case "1":
-    //         console.log("Case entered");
-    //         try{
-    //             res.render('areaAndPerimeter.ejs');
-    //         }
-    //         catch(err){
-    //             // console.log(err);
-    //             res.render("error.ejs");
-    //         }            
-    //         break;
-    //     case "2":
-    //         try{
-    //             res.render("billSpliter.ejs");
-    //         }
-    //         catch(err){
-    //             console.log(err);
-    //         }
-    //         break;
-    //     case "3":
-    //         try{
-    //             res.render("error.ejs");
-    //         }
-    //         catch(error){
-    //            console.log("nothing");
-    //         }
-    //         break;
-    //     case "4":
-    //         try{
-    //             res.render("")
-    //         }
-    //         catch(err){
-    //             console.log(err);
-    //         }
-    //         break;
-    //     case "5":
-    //         try{
-    //             req.render("")
-    //         }
-    //         catch(err){
-    //             console.log(err);
-    //         }
-    //         break;
-    //     case "6":
-    //         try{
-    //             console.log("Okay ");
-    //         }
-    //         catch(err){
-    //             console.error(err);
-    //         }
-    // }
-    res.send("This is should be your projects page");
+    let projectID = req.params.id;
+    switch(projectID){
+        case "1":
+            console.log("Case entered");
+            try{
+                res.render('areaAndPerimeter.ejs');
+            }
+            catch(err){
+                // console.log(err);
+                res.render("error.ejs");
+            }            
+            break;
+        case "2":
+            try{
+                res.render("billSpliter.ejs");
+            }
+            catch(err){
+                console.log(err);
+            }
+            break;
+        case "3":
+            try{
+                res.render("error.ejs");
+            }
+            catch(error){
+               console.log("nothing");
+            }
+            break;
+        case "4":
+            try{
+                res.render("")
+            }
+            catch(err){
+                console.log(err);
+            }
+            break;
+        case "5":
+            try{
+                req.render("")
+            }
+            catch(err){
+                console.log(err);
+            }
+            break;
+        case "6":
+            try{
+                console.log("Okay ");
+            }
+            catch(err){
+                console.error(err);
+            }
+    }
+    // res.send("This is should be your projects page");
 };
 module.exports = getprojectPageContorller;
